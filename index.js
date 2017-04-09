870cfaf8c54ee144a8ce72133df6a2bf4e13f20f
moritzmhmk <moritzmhmk@gmail.com>
2017-04-09 13:33:02 +0200

    parse OptionDescriptors

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const send = (msg, responseParser) => {
   })
 }
 
+
 class ReceivingBuffer {
   constructor (size) {
     this.size = size
@@ -45,22 +46,65 @@ class ReceivingBuffer {
   }
 }
 
+class ReceivingWordBuffer {
+  static get type () {
+    return {
+      'BOOL': 0,
+      'INT': 1,
+      'FIXED': 2
+    }
+  }
+  constructor (type) {
+    this.type = type
+    this.buffer = new ReceivingBuffer(4)
+  }
+  get complete () {
+    return this.buffer.complete
+  }
+  get data () {
+    let i = this.buffer.data.readInt32BE() // TODO signed or unsinged?
+    if (this.type === ReceivingWordBuffer.type.BOOL) { return i }
+    if (this.type === ReceivingWordBuffer.type.INT) { return i }
+    if (this.type === ReceivingWordBuffer.type.FIXED) { return i / (1 << 16) }
+    return i
+  }
+  sliceFrom (buf) {
+    buf = this.buffer.sliceFrom(buf)
+    return buf
+  }
+}
+
+class ReceivingDummyBuffer {
+  get complete () {
+    return true
+  }
+  get data () {
+    return null
+  }
+  sliceFrom (buf) {
+    return buf
+  }
+}
+
 class ReceivingStringBuffer {
   constructor () {
-    this.lengthBuffer = new ReceivingBuffer(4)
+    this.lengthBuffer = new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)
     this.stringBuffer
   }
   get complete () {
     return this.stringBuffer ? this.stringBuffer.complete : false
   }
   get data () {
-    return this.stringBuffer ? this.stringBuffer.buffer.toString() : undefined
+    if (!this.stringBuffer) { return undefined }
+    let str = this.stringBuffer.buffer.toString()
+    str = str.slice(-1) === '\0' ? str.slice(0, -1) : str
+    return str
   }
   sliceFrom (buf) {
     if (!this.lengthBuffer.complete) {
       buf = this.lengthBuffer.sliceFrom(buf)
       if (this.lengthBuffer.complete) {
-        this.stringBuffer = new ReceivingBuffer(this.lengthBuffer.buffer.readUInt32BE())
+        this.stringBuffer = new ReceivingBuffer(this.lengthBuffer.data)
       }
     }
     if (this.lengthBuffer.complete && !this.stringBuffer.complete) {
@@ -72,14 +116,14 @@ class ReceivingStringBuffer {
 
 class ReceivingPointerBuffer {
   constructor (pointerBuffer) {
-    this.isNullBuffer = new ReceivingBuffer(4)
+    this.isNullBuffer = new ReceivingWordBuffer(ReceivingWordBuffer.type.BOOL)
     this.pointerBuffer = pointerBuffer
   }
   get complete () {
     return this.isNull || this.pointerBuffer.complete
   }
   get isNull () {
-    return this.isNullBuffer.complete && this.isNullBuffer.buffer.readUInt32BE() === 1
+    return this.isNullBuffer.complete && this.isNullBuffer.data
   }
   get data () {
     return this.isNull ? null : this.pointerBuffer.data
@@ -122,7 +166,7 @@ class ReceivingStructBuffer {
 
 class ReceivingArrayBuffer {
   constructor (itemBufferCreator) {
-    this.lengthBuffer = new ReceivingBuffer(4)
+    this.lengthBuffer = new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)
     this.itemBufferCreator = itemBufferCreator
   }
   get complete () {
@@ -140,7 +184,7 @@ class ReceivingArrayBuffer {
   sliceFrom (buf) {
     if (!this.lengthBuffer.complete) {
       buf = this.lengthBuffer.sliceFrom(buf)
-      if (this.lengthBuffer.complete) { this.buffer = new Array(this.lengthBuffer.buffer.readUInt32BE()) }
+      if (this.lengthBuffer.complete) { this.buffer = new Array(this.lengthBuffer.data) }
     }
     for (let i = 0; i < this.buffer.length; i++) {
       if (!this.buffer[i]) {
@@ -149,6 +193,89 @@ class ReceivingArrayBuffer {
       if (!this.buffer[i].complete) {
         buf = this.buffer[i].sliceFrom(buf)
       }
+    }
+    return buf
+  }
+}
+
+class ReceivingConstraintBuffer {
+  constructor (rangeValueType) {
+    this.rangeValueType = rangeValueType
+    this.type = new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)
+  }
+  get complete () {
+    if (!this.type.complete) { return false }
+    if (!this.value.complete) { return false }
+    return true
+  }
+  get data () {
+    return {
+      type: this.type.data,
+      value: this.value ? this.value.data : undefined
+    }
+  }
+  sliceFrom (buf) {
+    if (!this.type.complete) {
+      buf = this.type.sliceFrom(buf)
+      if (this.type.complete) {
+        if (this.type.data === 0) { // NONE
+          this.value = new ReceivingDummyBuffer()
+        }
+        if (this.type.data === 1) {// RANGE
+          this.value = new ReceivingPointerBuffer(new ReceivingStructBuffer([
+            {name: 'min', buffer: new ReceivingWordBuffer(this.rangeValueType)},
+            {name: 'max', buffer: new ReceivingWordBuffer(this.rangeValueType)},
+            {name: 'quantization', buffer: new ReceivingWordBuffer(this.rangeValueType)}
+          ]))
+        }
+        if (this.type.data === 2) {// WORD_LIST
+          this.value = new ReceivingArrayBuffer((index) => { return new ReceivingWordBuffer(ReceivingWordBuffer.type.INT) })
+        }
+        if (this.type.data === 3) {// STRING_LIST
+          this.value = new ReceivingArrayBuffer((index) => { return new ReceivingStringBuffer() })
+        }
+      }
+    }
+
+    if (this.type.complete) {
+      buf = this.value.sliceFrom(buf)
+    }
+    console.log(this.complete, this.data)
+
+    return buf
+  }
+}
+
+class ReceivingOptionDescriptorBuffer {
+  constructor () {
+    this.structBuffer = new ReceivingStructBuffer([
+      {name: 'name', buffer: new ReceivingStringBuffer()},
+      {name: 'title', buffer: new ReceivingStringBuffer()},
+      {name: 'description', buffer: new ReceivingStringBuffer()},
+      {name: 'type', buffer: new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)},
+      {name: 'units', buffer: new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)},
+      {name: 'size', buffer: new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)},
+      {name: 'cap', buffer: new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)}
+    ])
+    this.constraintBuffer = new ReceivingConstraintBuffer()
+  }
+  get complete () {
+    return this.structBuffer.complete && this.constraintBuffer && this.constraintBuffer.complete
+  }
+  get data () {
+    let data = Object.assign({}, this.structBuffer.data)
+    data.constraint = this.constraintBuffer.data
+    return data
+  }
+  sliceFrom (buf) {
+    if (!this.structBuffer.complete) {
+      buf = this.structBuffer.sliceFrom(buf)
+      if (this.structBuffer.complete) {
+        this.constraintBuffer = new ReceivingConstraintBuffer(this.structBuffer.data.type)
+      }
+    }
+    if (this.structBuffer.complete) {
+      buf = this.constraintBuffer.sliceFrom(buf)
     }
     return buf
   }
@@ -173,14 +300,13 @@ class GetDevicesParser {
     this.buffer = {}
     this.buffer.devices = new ReceivingArrayBuffer((index) => {
       return new ReceivingPointerBuffer(new ReceivingStructBuffer([
-        // {name: '_', buffer: new ReceivingBuffer(4)}, // TODO this word is 1 if this is a NULL Pointer or 0 otherwise
         {name: 'name', buffer: new ReceivingStringBuffer()},
         {name: 'vendor', buffer: new ReceivingStringBuffer()},
         {name: 'model', buffer: new ReceivingStringBuffer()},
         {name: 'type', buffer: new ReceivingStringBuffer()}
       ]))
     })
-    this.buffer.status = new ReceivingBuffer(4)
+    this.buffer.status = new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)
   }
   parse (data) {
     if (!this.buffer.status.complete) {
@@ -203,7 +329,7 @@ class GetDevicesParser {
 class OpenParser {
   constructor () {
     this.buffer = new ReceivingStructBuffer([
-      {name: 'status', buffer: new ReceivingBuffer(4)},
+      {name: 'status', buffer: new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)},
       {name: 'handle', buffer: new ReceivingBuffer(4)},
       {name: 'resource', buffer: new ReceivingStringBuffer()}
     ])
@@ -219,7 +345,7 @@ class OpenParser {
 
 class AuthorizeParser {
   constructor (originalParser) {
-    this.status = new ReceivingBuffer(4)
+    this.status = new ReceivingWordBuffer(ReceivingWordBuffer.type.INT)
     this.originalParser = originalParser
   }
   parse (data) {
@@ -228,6 +354,28 @@ class AuthorizeParser {
   }
   get data () {
     return this.originalParser.data // TODO handle failed authorization
+  }
+}
+
+class GetOptionDescriptorsParser {
+  constructor () {
+    this.buffer = {}
+    this.buffer.optionDescriptors = new ReceivingArrayBuffer((index) => {
+      return new ReceivingPointerBuffer(new ReceivingOptionDescriptorBuffer())
+    })
+  }
+  parse (data) {
+    //if (!this.buffer.status.complete) {
+    //  data = this.buffer.status.sliceFrom(data)
+    //}
+    if (!this.buffer.optionDescriptors.complete) {
+      data = this.buffer.optionDescriptors.sliceFrom(data)
+    }
+    // console.log(require('util').inspect(this.buffer.optionDescriptors, false, 10))
+    return this.buffer.optionDescriptors.complete ? data : undefined
+  }
+  get data () {
+    return this.buffer.optionDescriptors.data
   }
 }
 
@@ -261,7 +409,6 @@ client.connect(6566, '127.0.0.1', () => {
     var resource = sanetypes.string(data.resource)
     var username = sanetypes.string('moritz')
     var salt = data.resource.split('$MD5$')[1]
-    salt = salt.slice(0, -1) // remove trailing \0
     var password = sanetypes.string('$MD5$' + md5(salt + 'test'))
     var buf = Buffer.concat([rpcCode, resource, username, password])
     return send(buf, new AuthorizeParser(new OpenParser()))
@@ -272,10 +419,10 @@ client.connect(6566, '127.0.0.1', () => {
     rpcCode.writeUInt32BE(sanetypes.rpc.SANE_NET_GET_OPTION_DESCRIPTORS)
     var handle = data.handle
     var buf = Buffer.concat([rpcCode, handle])
-    return send(buf, new FakeParser())
+    return send(buf, new GetOptionDescriptorsParser())
   })
   .then((data) => {
-    console.log('get option descriptors response', data)
+    console.log('get option descriptors response', require('util').inspect(data, false, 10))
   })
   .catch((reason) => { console.log('promise rejected', reason) })
   // client.write(new Buffer([0, 0, 0, 1])
