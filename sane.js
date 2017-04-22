b55c76156f6a986ded962756dacb70fb9cd051ed
moritzmhmk <moritzmhmk@gmail.com>
2017-04-23 01:29:17 +0200

    improved jsdoc documentation

--- sane.js
+++ sane.js
@@ -246,8 +246,8 @@ class SaneBuffer {
 /**
  * A byte is encoded as an 8 bit value.
  * Since the transport protocol is assumed to be byte-orientd, the bit order is irrelevant.
- *
- * @param size number of bytes - defaults to one
+ * @extends SaneBuffer
+ * @param {number} size - number of bytes (defaults to one)
  */
 class SaneBytes extends SaneBuffer {
   constructor (size) {
@@ -272,8 +272,8 @@ class SaneBytes extends SaneBuffer {
 /**
  * A word is encoded as 4 bytes (32 bits).
  * The bytes are ordered from most-significant to least-significant byte (big-endian byte-order).
- *
- * @param type type of value this word encodes see TODO
+ * @extends SaneBuffer
+ * @param {enums.status} type - type of value this word encodes
  */
 class SaneWord extends SaneBuffer {
   constructor (type) {
@@ -320,7 +320,7 @@ class SaneChar extends SaneBuffer {
  * The word is 0 in case of a non-NULL pointer (sic!).
  * In the case of a NULL pointer, no bytes are encoded for the pointer value.
  *
- * @param pointerBuffer the buffer to store the value of the pointer
+ * @param {SaneBuffer} pointerBuffer - the buffer to store the value of the pointer
  */
 class SanePointer extends SaneBuffer {
   constructor (pointerBuffer) {
@@ -357,7 +357,7 @@ class SanePointer extends SaneBuffer {
 /**
  * A structure is encoded by simply encoding the structure members in the order in which they appear.
  *
- * @param structDefinition definition of the struct in the format [ {name: 'name', bufferCreator: () => new SaneBuffer()}, ...] // TODO
+ * @param {Map} structDefinition definition of the struct (String => SaneBuffer)
  */
 class SaneStructure extends SaneBuffer {
   constructor (definitionMap) {
@@ -395,7 +395,7 @@ class SaneStructure extends SaneBuffer {
  * followed by the values of the elements in the array.
  * The length may be zero in which case no bytes are encoded for the element values.
  *
- * @param itemBufferCreator function that returns a new SaneBuffer. Called for every array element.
+ * @param {function} itemBufferCreator should return a new SaneBuffer, called for every array element.
  */
 class SaneArray extends SaneBuffer {
   constructor (itemBufferCreator) {
