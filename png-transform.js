a39c9bfead915f44378fa5f18c8f3a0f8017c02a
moritzmhmk <moritzmhmk@gmail.com>
2017-04-28 20:15:59 +0200

    moved PNGTransform to png-transform.js

--- png-transform.js
+++ png-transform.js
@@ -1,71 +1,7 @@
-const net = require('net')
-const path = require('path')
-const fs = require('fs')
-
-const crc32 = require('buffer-crc32')
+const Transform = require('stream').Transform
 var zlib = require('zlib')
 
-const sane = require('./sane')
-
-let width, height, depth, format
-
-let saneClient = new sane.Socket()
-saneClient.on('authorize', (backend, callback) => {
-  console.log(`backend "${backend}" requires authorization`)
-  callback('moritz', 'test')
-})
-saneClient.connect(6566, '127.0.0.1')
-.then(() => saneClient.init())
-.then((data) => {
-  console.log('init response', data)
-  return saneClient.getDevices()
-})
-.then((data) => {
-  console.log('get devices response', data)
-  return saneClient.open(data[0].name)
-})
-.then((data) => {
-  console.log('open device response', data)
-  return saneClient.getOptionDescriptors(data)
-})
-.then((data) => {
-  console.log('get option descriptors response', data)
-  var handle = 0 // TODO use actual handle
-  var option = 7
-  var action = 'SET_VALUE'
-  var valueType = data[option].type
-  var value = 120
-  return saneClient.controlOption(handle, option, action, value, valueType)
-})
-.then((data) => {
-  console.log('control option response', data)
-  var handle = 0 // TODO use actual handle
-  return saneClient.getParameters(handle)
-})
-.then((data) => {
-  console.log('get parameters response', data)
-  width = data.pixelsPerLine
-  height = data.lines
-  depth = data.depth
-  format = data.format
-  var handle = 0 // TODO use actual handle
-  return saneClient.start(handle)
-})
-.then((data) => {
-  console.log('start response', data)
-  var png = fs.createWriteStream(path.join(__dirname, '/scan.png'))
-  console.log(width, height)
-  var saneTransform = new sane.ImageTransform()
-  var pngTransform = new PNGTransform(width, height, depth, format)
-
-  var dataSocket = new net.Socket()
-  dataSocket.pipe(saneTransform).pipe(pngTransform).pipe(png)
-  dataSocket.on('error', (err) => { console.log(err) })
-  dataSocket.connect(data.port, '127.0.0.1')
-})
-.catch((reason) => { console.log('promise rejected', reason) })
-
-const Transform = require('stream').Transform
+const crc32 = require('buffer-crc32')
 
 class PNGTransform extends Transform {
   constructor (width, height, depth, format) {
@@ -112,13 +48,9 @@ class PNGTransform extends Transform {
         return Buffer.concat([filter, data])
       }
     }
-    // var output = pako.deflate(readLine(data))
     this._zlib.write(readLine(data), done)
-    // console.log('->writePixels', lines)
-    // this.writePNGChunk('IDAT', output)
   }
   _transform (data, encoding, done) {
-    // console.log('->transform')
     if (!this.headerWritten) {
       this.headerWritten = true
       this.push(new Buffer([ 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a ])) // png magic number
@@ -142,3 +74,5 @@ class PNGTransform extends Transform {
     this._zlib.end()
   }
 }
+
+module.exports = PNGTransform
